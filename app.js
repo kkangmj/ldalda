21d1dd525b7cfc9ea1c8d19fb0b66c1c63981158
xrabcde <joara124@ewhain.net>
2020-09-08 16:39:14 +0900

    네이버 로그인 동의화면까지 구현

--- app.js
+++ app.js
@@ -1,41 +1,82 @@
-var createError = require('http-errors');
-import express from "express";
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var createError = require('http-errors');
+// import express from "express";
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+//
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+//
+// var app = express();
+//
+// var passport = require('passport');
+//
+// app.use(passport.initialize());
+// app.use(passport.session());
+//
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+//
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+//
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+//
+// module.exports = app;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var express = require('express');
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var client_id = '58cYYnBqS2RpaFeuv68P';
+var client_secret = 'nYXsaYnTwu';
+var state = "RAMDOM_STATE";
+var redirectURI = encodeURI("http://127.0.0.1:3000");
+var api_url = "";
+app.get('/naverlogin', function (req, res) {
+  api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
+  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+  res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/callback', function (req, res) {
+  code = req.query.code;
+  state = req.query.state;
+  api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+      + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
+  var request = require('request');
+  var options = {
+    url: api_url,
+    headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+  };
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log('error = ' + response.statusCode);
+    }
+  });
 });
-
-module.exports = app;
+app.listen(3000, function () {
+  console.log('http://127.0.0.1:3000/naverlogin app listening on port 3000!');
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport'); //passport 추가
+var NaverStrategy = require('passport-naver').Strategy;
+
+import config from '../config';
+
+//처리 후 callback 처리 부분 성공/실패 시 리다이렉트 설정
+router.get('/naver/callback', passport.authenticate('naver', {
+      successRedirect : '/',
+      failureRedirect : '/main/login'
+    })
+);
+
+//별도 config 파일에 '네아로'에 신청한 정보 입력
+passport.use(new NaverStrategy({
+      clientID: config.federation.naver.clientId,
+      clientSecret: config.federation.naver.clientSecret,
+      callbackURL: config.federation.naver.callbackURL
+    },
+    function (accessToken, refreshToken, profile, done) {
+      process.nextTick(function() {
+        var user = {
+          name : profile.displayName,
+          email : profile.emails[0].value,
+          username : profile.displayName,
+          provider : 'naver',
+          naver ; profile._json
+        };
+        console.log("user=");
+        console.log(user);
+
+        return done(null, user);
+      });
+    }
+    ));
+
+//failed to serialize user into session 에러 발생 시 아래의 내용을 추가 한다.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(req, user, done) {
+  //passport로 로그인 처리 후 해당 정보를 session에 담는다.
+  seq.session.sid = user.name;
+  console.log("Session Check :"+req.session.sid);
+  done(null, user);
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//naver 로그인
+router.get('/auth/login/naver',
+    passport.authenticate('naver')
+);
+//naver 로그인 연동 콜백
+router.get('http://127.0.0.1:3000',
+    passport.authenticate('naver', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+    })
+);
 
 module.exports = router;
